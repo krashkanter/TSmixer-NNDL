eb045d14d85140e0c55976452de1089f0cea676f
Keerthan K <kkeerthan2005@gmail.com>
2025-10-17 07:53:07 +0000

    datasets?

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import pandas as pd
+import numpy as np
+
+
 def main():
-    print("Hello from tsmixer-nndl!")
+    data = pd.read_csv("datasets/559_train.csv")
+    df = pd.DataFrame(data)
+    df.head()
 
 
 if __name__ == "__main__":
